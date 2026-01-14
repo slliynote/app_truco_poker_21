5840648d1d2ccb8eb4d687e1a9fe3feaed218165
slliynote <slliynotenote@gmail.com>
2026-01-14 00:18:57 -0300

    feat: menu de tela feito

--- java/com/example/pokeraplication/MainActivity.java
+++ java/com/example/pokeraplication/MainActivity.java
@@ -8,17 +8,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fabAudio;
+    boolean audioLigado = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        fabAudio = findViewById(R.id.floatingActionButton);
+
+
+        fabAudio.setOnClickListener(v -> {
+            if (audioLigado) {
+                fabAudio.setImageResource(R.drawable.audiosim); // muda ícone
+                // aqui você pausa ou muta o áudio
+            } else {
+                fabAudio.setImageResource(R.drawable.audionao); // volta ícone
+                // aqui você toca ou desmuta o áudio
+            }audioLigado = !audioLigado;
+
         });
     }
-}
+
+};
+
