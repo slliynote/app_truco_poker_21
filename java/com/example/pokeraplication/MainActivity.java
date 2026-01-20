9311cff871573e4e8c0e04c875e7c51f21d70a4d
slliynote <slliynotenote@gmail.com>
2026-01-20 20:41:26 -0300

    feat: iniciado o selectruco e poker

--- java/com/example/pokeraplication/MainActivity.java
+++ java/com/example/pokeraplication/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.pokeraplication;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
+
+
 }
--- java/com/example/pokeraplication/SegundaFragment.java
+++ java/com/example/pokeraplication/SegundaFragment.java
@@ -1,9 +1,11 @@
 package com.example.pokeraplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,7 +14,7 @@ import androidx.fragment.app.Fragment;
 public class SegundaFragment extends Fragment {
 
     public SegundaFragment() {
-        // construtor vazio obrigatório
+        // Construtor vazio obrigatório
     }
 
     @Nullable
@@ -22,7 +24,18 @@ public class SegundaFragment extends Fragment {
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState
     ) {
-        // AQUI você liga o Fragment ao XML
-        return inflater.inflate(R.layout.segundatelastart, container, false);
+        // Inflando o layout do Fragment
+        View root = inflater.inflate(R.layout.segundatelastart, container, false);
+
+        // Pegando a ImageView dentro do layout inflado
+        ImageView imgBotao = root.findViewById(R.id.btnCarta1);
+
+        // Adicionando o clique
+        imgBotao.setOnClickListener(v -> {
+            Log.d("TESTE", "Imagem clicada");
+        });
+
+        // Retorna a view inflada
+        return root;
     }
 }
